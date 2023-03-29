c71ab9c97415c78327500cd12c69df8d077f3769
Руслан Аитов <r_aitov@mail.ru>
2023-03-29 21:32:47 +0300

    task19-21

---
+++ task19-21/2k.py
@@ -0,0 +1,21 @@
+win = 77
+start = 7
+
+
+def f(a, b, c, m):
+    if a + b >= win:
+        return c % 2 == m % 2
+    if c == m:
+        return False
+    moves = [f(a + 1, b, c + 1, m),
+             f(a, b + 1, c + 1, m),
+             f(a * 2, b, c + 1, m),
+             f(a, b * 2, c + 1, m)]
+    return any(moves) if (c + 1) % 2 == m % 2 else all(moves)
+
+
+for b in range(1, win - start):
+    for m in range(1, 4 + 1):
+        if f(start, b, 0, m):
+            print(b, m)
+            break
